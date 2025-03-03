59e07b6da6af5a2c265e8adff128e725fe1c744c
Adrian Markefka <am97veb@gmail.com>
2025-03-03 12:53:11 +0100

    ThemeProvider added

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    colors: {
+        white: "#FFFFFF",
+        whiteLilac: "#FBFBFE",
+        slateGray: "#6E7E91",
+        mineShaft: "#252525",
+        scienceBlue: "#0366D6",
+    },
+}
