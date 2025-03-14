a28f82f77b477b61aab238553a3999b37600088e
Adrian Markefka <am97veb@gmail.com>
2025-03-14 09:13:35 +0100

    changed margin and deleted unnecesery spaces

--- src/Section/styled.js
+++ src/Section/styled.js
@@ -3,10 +3,10 @@ import styled from "styled-components";
 export const StyledSection = styled.section`
     max-width: 1216px;
     background-color: ${({ theme }) => theme.colors.white};
-    margin: 72px auto;
+    margin-bottom: 72px;
     padding: 32px;
     box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03),
-                 0px -2px 50px 0px rgba(9, 10, 51, 0.02);
+                0px -2px 50px 0px rgba(9, 10, 51, 0.02);
 `;
 
 export const Header = styled.h2`
@@ -22,10 +22,7 @@ export const List = styled.ul`
     column-count: 3;
     padding: 32px 0 0 0;
     margin: 0;
-    
     list-style-type: none;
-    
-    
 `;
 
 export const ListItem = styled.li`
