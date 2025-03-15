121747bf6a74c14309fad3609c4bb20cba001463
Adrian Markefka <am97veb@gmail.com>
2025-03-15 13:03:57 +0100

    changed some icon names and deleted unnecessery spaces

--- src/Portfolio/index.js
+++ src/Portfolio/index.js
@@ -1,58 +1,61 @@
-import { ReactComponent as GitHub } from "./svg/github.svg";
-import { ReactComponent as Danger } from "./svg/danger.svg";
-import { PortfolioSection, Header, List, LeadParagraph, StyledHeader, Title, ProjactsSection, Project, ProjectName, ProjectDescription, Projects, Label, Link, LinkContainer, ErrorHeader, ErrorParagraph, LoadingInformation, LoadingIcon, Jakiśchuj, Wrapper } from "./styled";
+import { PortfolioSection, Header, LeadParagraph, Title, Project, ProjectName, ProjectDescription, Projects, Link, ProjectLinks, ErrorHeader, LoadingInformation, LoadingIcon, Wrapper } from "./styled";
+import { ReactComponent as GitHubIcon } from "./svg/github.svg";
+import { ReactComponent as DangerIcon } from "./svg/danger.svg";
 import { useProjectsDownloads } from "./useProjectDownloads";
+import { LinkContainer } from "../LinkContainer";
 
+export const Portfolio = ({ title, gitHubLink }) => {
+  const dataState = useProjectsDownloads();
+  const projects = dataState.projectsData;
 
-export const Portfolio = ({ title, gitHubButton, gitHubLink }) => {
-    const dataState = useProjectsDownloads();
-    const projects = dataState.projectsData;
+  return (
+    <PortfolioSection>
+      <Header>
+        <GitHubIcon />
+        <Title>{title}</Title>
+        <LeadParagraph>My recent projects</LeadParagraph>
+      </Header>
+      {dataState.status === "loading"
+        ?
+        (
+          <Wrapper>
+            <LoadingInformation>Please wait, projects are being loaded...</LoadingInformation>
+            <LoadingIcon />
+          </ Wrapper>
+        )
+        : dataState.status === "error"
+          ?
+          (
+            <Wrapper>
+              <DangerIcon />
+              <ErrorHeader>Ooops! Something went wrong...</ErrorHeader>
+              <LoadingInformation>Sorry, failed to load Github projects.
+                <br />
+                You can check them directly on Github.</LoadingInformation>
+              <LinkContainer>
+                {gitHubLink}
+              </LinkContainer>
+            </ Wrapper>
+          )
+          :
+          (
+            <Projects>
+              {projects.map(project =>
+                <Project>
+                  <ProjectName>{project.name.replaceAll("-", " ")}</ProjectName>
+                  <ProjectDescription>{project.description}</ProjectDescription>
+                  <ProjectLinks>
+                    <span>Demo:</span>
+                    <Link href={project.homepage}> {project.homepage}</Link>
+                    <span>Code:</span>
+                    <Link href={project.html_url}> {project.html_url}</Link>
+                  </ProjectLinks>
+                </Project>
+              )}
+            </Projects>
+          )
+      }
 
-    return (
-        <PortfolioSection>
-            <Header>
-                <GitHub />
-                <Title>{title}</Title>
-                <LeadParagraph>My recent projects</LeadParagraph>
-            </Header>
-            {dataState.status === "loading"
-                ?
-                (
-                    <Wrapper>
-                        <LoadingInformation>Please wait, projects are being loaded...</LoadingInformation>
-                        <LoadingIcon />
-                    </ Wrapper>
-                )
-                : dataState.status === "error"
-                    ?
-                    (
-                        <Wrapper>
-                            <Danger />
-                            <ErrorHeader>Ooops! Something went wrong...</ErrorHeader>
-                            <LoadingInformation>Sorry, failed to load Github projects.
-                                <br />
-                                You can check them directly on Github.</LoadingInformation>
-                            {gitHubButton}
-                        </ Wrapper>
-                    )
-                    :
-                    (<Projects>
-                        {projects.map(project =>
-                            <Project>
-                                <ProjectName>{project.name.replaceAll("-", " ")}</ProjectName>
-                                <ProjectDescription>{project.description}</ProjectDescription>
-                                <LinkContainer>
-                                    <span>Demo:</span>
-                                    <Link href={project.homepage}> {project.homepage}</Link>
-                                    <span>Code:</span>
-                                    <Link href={project.html_url}> {project.html_url}</Link>
-                                </LinkContainer>
-                            </Project>
-                        )}
-                    </Projects>
-                    )
-            }
-
-        </PortfolioSection>
-    )
+    </PortfolioSection>
+  )
 };
