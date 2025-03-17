41c746d706fc47a9ac7b6bd90b7d291feb9682dd
Adrian Markefka <am97veb@gmail.com>
2025-03-17 09:47:41 +0100

    improving code readability, added description to index.js and deleted contactDescription file

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,17 +1,32 @@
-import { Caption, ContactContainer, Description, Mail, StyledFacebook, StyledGitHub, StyledInstagram, StyledLinkedin, StyledX, Title, Wrapper } from "./styled";
-import { contactDescription } from "./contactDescription";
+import { Caption, ContactContainer, Description, Mail, StyledFacebook, StyledGitHub, StyledInstagram, StyledLinkedin, StyledX, Wrapper } from "./styled";
 
 export const Footer = ({ mail }) => (
     <Wrapper>
-        <Caption>let's talk</Caption>
-        <Mail>{mail}</Mail>
-        <Description>{contactDescription}</Description>
+        <Caption>
+            let's talk
+        </Caption>
+        <Mail>
+            {mail}
+        </Mail>
+        <Description>
+            If you need help with your ideas for a website or app - go ahead, I'm always open to new projects and new challenges. Don't be shy, feel free to contact me!
+        </Description>
         <ContactContainer>
-            <a href="https://github.com/am97veb"><StyledGitHub /></a>
-            <a href="https://www.linkedin.com/in/am97veb/"><StyledLinkedin /></a>
-            <a href="https://www.facebook.com/adrian.markefka.5"><StyledFacebook /></a>
-            <a href="https://www.instagram.com/am97veb/"><StyledInstagram /></a>
-            <a href="https://x.com/AM97VEB"><StyledX /></a>
+            <a href="https://github.com/am97veb">
+                <StyledGitHub />
+            </a>
+            <a href="https://www.linkedin.com/in/am97veb/">
+                <StyledLinkedin />
+            </a>
+            <a href="https://www.facebook.com/adrian.markefka.5">
+                <StyledFacebook />
+            </a>
+            <a href="https://www.instagram.com/am97veb/">
+                <StyledInstagram />
+            </a>
+            <a href="https://x.com/AM97VEB">
+                <StyledX />
+            </a>
         </ContactContainer>
     </Wrapper>
 );
