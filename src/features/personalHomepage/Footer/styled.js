a5310dc640d51f616f0ab37da21a6e0135ddf6a8
Adrian Markefka <am97veb@gmail.com>
2025-03-21 19:52:19 +0100

    changes to element styling and renaming of svg icons folder

--- src/features/personalHomepage/Footer/styled.js
+++ src/features/personalHomepage/Footer/styled.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-import { ReactComponent as GitHub } from "./contactIcons/gitHub_black.svg";
-import { ReactComponent as Facebook } from "./contactIcons/facebook_icons_black.svg";
-import { ReactComponent as Instagram } from "./contactIcons/Instagram_icon_black.svg";
-import { ReactComponent as Linkedin } from "./contactIcons/LinkedIN.svg";
-import { ReactComponent as X } from "./contactIcons/xicon_black.svg";
+import { ReactComponent as GitHub } from "./socialMediaIcons/gitHub_black.svg";
+import { ReactComponent as Facebook } from "./socialMediaIcons/facebook_icons_black.svg";
+import { ReactComponent as Instagram } from "./socialMediaIcons/Instagram_icon_black.svg";
+import { ReactComponent as Linkedin } from "./socialMediaIcons/LinkedIN.svg";
+import { ReactComponent as X } from "./socialMediaIcons/xicon_black.svg";
 
 export const Wrapper = styled.div`
   align-self: center;
@@ -15,19 +15,22 @@ export const Caption = styled.p`
     font-size: 12px;
     font-weight: 700;
     line-height: 15.6px;
-    color: ${({ theme }) => theme.colors.slateGray};
+    color: ${({ theme }) => theme.colors.caption};
     text-transform: uppercase;
     letter-spacing: 0px;
+    margin: 0 0 24px;
 `;
 
-export const Mail = styled.div`
+export const Mail = styled.span`
+    display: inline-block;
     font-size: 32px;
     font-weight: 900;
     line-height: 45.99px;
-    margin: 24px 0;
+    margin: 0 0 24px;
 
     &:hover {
-        color: ${({theme}) => theme.colors.scienceBlue}
+        transition: 0.5s;
+        color: ${({theme}) => theme.colors.afterHovering}
     }
 `;
 
@@ -35,8 +38,7 @@ export const Description = styled.p`
     font-size: 20px;
     font-weight: 400;
     line-height: 28px;
-    color: ${({ theme }) => theme.colors.mineShaft};
-    margin-bottom: 56px;
+    margin: 0 0 56px;
 `;
 
 export const ContactContainer = styled.div`
@@ -45,36 +47,60 @@ export const ContactContainer = styled.div`
 `;
 
 export const StyledGitHub = styled(GitHub)`
-    
+    path {
+        fill: ${({theme}) => theme.colors.icon}
+    }
+
     &:hover path {
-        fill: ${({theme})=> theme.colors.scienceBlue};
+        transition: 0.5s;
+        fill: ${({theme})=> theme.colors.afterHovering};
     }
 `;
 
 export const StyledLinkedin = styled(Linkedin)`
+path {
+        fill: ${({theme}) => theme.colors.icon}
+    }
 
     &:hover path {
-        fill: ${({theme})=> theme.colors.scienceBlue};
+        transition: 0.5s;
+        fill: ${({theme})=> theme.colors.afterHovering};
     }
 `;
 
 export const StyledFacebook = styled(Facebook)`
-    
+    path {
+        fill: ${({theme}) => theme.colors.icon}
+    }
+
     &:hover path {
-        fill: ${({theme})=> theme.colors.scienceBlue};
+        transition: 0.5s;
+        fill: ${({theme})=> theme.colors.afterHovering};
     }
 `;
 
 export const StyledInstagram = styled(Instagram)`
-    
+    path {
+        fill: ${({theme}) => theme.colors.icon}
+    }
+
     &:hover path {
-        fill: ${({theme})=> theme.colors.scienceBlue};
+        transition: 0.5s;
+        fill: ${({theme})=> theme.colors.afterHovering};
     }
 `;
 
 export const StyledX = styled(X)`
+    [id="1"] {
+        fill: ${({theme}) => theme.colors.icon}
+    }
 
-    &:hover [id] {
-        fill: ${({theme})=> theme.colors.scienceBlue};
+    [id="2"] {
+        fill: ${({theme}) => theme.colors.bodyBackground}
+    } 
+
+    &:hover [id="1"] {
+        transition: 0.5s;
+        fill: ${({theme})=> theme.colors.afterHovering};
     }
 `;
