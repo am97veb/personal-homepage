ef1a3f99744322397506e59f61be4b31fe0ed00e
Adrian Markefka <am97veb@gmail.com>
2025-03-21 19:24:49 +0100

    renamed colours and created colour objects for the light and dark theme

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,42 @@
-export const theme = {
+export const lightTheme = {
     colors: {
-        white: "#FFFFFF",
-        whiteLilac: "#FBFBFE",
-        slateGray: "#6E7E91",
-        mineShaft: "#252525",
-        scienceBlue: "#0366D6",
+        bodyBackground: "#FBFBFE",
+        bodyText: "#252525",
+        caption: "#6E7E91",
+        header: "#252525",
+        aboutMeContent: "#6E7E91",
+        borderBottom: "#D1D5DA4D",
+        sectionBackground: "#FFFFFF",
+        sectionShadowTop: "#090A3308",
+        sectionShadowBottom: "#090A3305",
+        sectionContent: "#6E7E91",
+        buttonContent: "#FFFFFF",
+        linkContainer: "#0366D6",
+        projectHeader: "#0366D6",
+        projectDescription: "#6E7E91",
+        link: "#0366D6",
+        afterHovering: "#0366D6",
+    },
+}
+
+export const darkTheme = {
+    colors: {
+        bodyBackground: "#252525",
+        bodyText: "#FFFFFF",
+        caption: "#FFFFFF",
+        header: "#FFFFFF",
+        aboutMeContent: "#FFFFFF",
+        borderBottom: "#D1D5DA4D",
+        sectionBackground: "#363636B8",
+        sectionShadowTop: "#090A3308",
+        sectionShadowBottom: "#090A3305",
+        sectionContent: "#FFFFFF",
+        buttonContent: "#FFFFFF",
+        linkContainer: "#2188FF",
+        projectHeader: "#FFFFFF",
+        projectDescription: "#FFFFFF",
+        link: "#2188FF",
+        afterHovering: "#2188FF",
+        icon: "#FFFFFF",
     },
 }
