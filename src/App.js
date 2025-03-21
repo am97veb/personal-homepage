b4a04d7de887cb9814ecdfe649b7a560e68694c9
Adrian Markefka <am97veb@gmail.com>
2025-03-21 19:15:28 +0100

    moved ThemeProvider and GlobalStyle to App.js

--- src/App.js
+++ src/App.js
@@ -8,43 +8,54 @@ import { nextToLearn } from "./features/personalHomepage/nextToLearn.js";
 import { Portfolio } from "./features/personalHomepage/Portfolio/index.js";
 import { ToggleTheme } from "./ToggleTheme/index.js";
 import { Footer } from "./features/personalHomepage/Footer/index.js";
-import { ReactComponent as Massage } from "./Message.svg";
-import skillsIcon from "./skillsIcon.png";
-import nextToLearnIcon from "./nextToLearnIcon.png";
+import { ReactComponent as Massage } from "./common/Message.svg";
+import skillsIcon from "./features/personalHomepage/skillsIcon.png";
+import nextToLearnIcon from "./features/personalHomepage/nextToLearnIcon.png";
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyle } from './GlobalStyle';
+import { selectTheme } from "./themeSlice.js";
+import { useSelector } from "react-redux";
 
 function App() {
+  const darkMode = useSelector(selectTheme);
+  const theme = darkMode ? darkTheme : lightTheme;
+
   return (
-    <Container>
-      <Header
-        mail={<Link
-          icon={<Massage />}
-          content="Hire Me"
-          adress={mailTo}
-        />}
-        toggleTheme={<ToggleTheme />}
-      />
-      <Section
-        title="My skills includes"
-        icon={<img src={skillsIcon}/>}
-        content={skills}
-      />
-      <Section
-        title="What I want to learn next"
-        icon={<img src={nextToLearnIcon}/>}
-        content={nextToLearn}
-      />
-      <Portfolio
-        gitHubLink={<Link
-          adress="https://github.com/am97veb"
-          content="Go to GitHub"
-        />}
-      />
-      <Footer
-        mail={<Link
-          content="am97veb@gmail.com"
-          adress={mailTo} />}
-      />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Container>
+        <Header
+          mail={<Link
+            icon={<Massage />}
+            content="Hire Me"
+            adress={mailTo}
+          />}
+          toggleTheme={<ToggleTheme />}
+        />
+        <Section
+          title="My skills includes"
+          icon={<img src={skillsIcon} />}
+          content={skills}
+        />
+        <Section
+          title="What I want to learn next"
+          icon={<img src={nextToLearnIcon} />}
+          content={nextToLearn}
+        />
+        <Portfolio
+          gitHubLink={<Link
+            adress="https://github.com/am97veb"
+            content="Go to GitHub"
+          />}
+        />
+        <Footer
+          mail={<Link
+            content="am97veb@gmail.com"
+            adress={mailTo} />}
+        />
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { store } from "./store.js"
-import { ThemeProvider } from 'styled-components';
-import { theme } from './theme';
-import { GlobalStyle } from './GlobalStyle';
+import store from './store.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
         <App />
-      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
