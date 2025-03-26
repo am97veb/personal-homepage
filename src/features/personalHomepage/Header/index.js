e29b021123842724cc3b0c0f02b686fc0f0e825b
Adrian Markefka <am97veb@gmail.com>
2025-03-26 15:49:20 +0100

    LinkContainer deleted

--- src/features/personalHomepage/Header/index.js
+++ src/features/personalHomepage/Header/index.js
@@ -1,6 +1,5 @@
 import { Wrapper, ImageContainer, Image, AboutMe, Caption, StyledHeader, Description } from "./styled";
 import adrianMarkefka from "./AdrianMarkefka.jpg"
-import { LinkContainer } from "../LinkContainer";
 
 export const Header = ({ mail, toggleTheme }) => (
     <Wrapper>
@@ -17,9 +16,7 @@ export const Header = ({ mail, toggleTheme }) => (
             <Description>
                 💻😀 I'm Frontend Developer and I'm looking for a job. I thought what I wanted to do in life for a long time. My search led me to the IT industry and I finally felt what I want to do - work as a Frontend Developer. You can see what I have already learned below, what I would like to learn in the near future and my finished projects. Feel free to contact me 📧
             </Description>
-            <LinkContainer>
                 {mail}
-            </LinkContainer>
         </AboutMe>
         {toggleTheme}
     </Wrapper>
