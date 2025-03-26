7ba1e878eb20a44a9ee29ef1ac8fa2f40cb00744
Adrian Markefka <am97veb@gmail.com>
2025-03-26 15:53:36 +0100

    created object array with links and stylised icons, rendered view and added rel and target attributes

--- src/features/personalHomepage/Footer/index.js
+++ src/features/personalHomepage/Footer/index.js
@@ -1,32 +1,39 @@
+import { mailTo } from "../../../mailTo";
 import { Caption, ContactContainer, Description, Mail, StyledFacebook, StyledGitHub, StyledInstagram, StyledLinkedin, StyledX, Wrapper } from "./styled";
 
-export const Footer = ({ mail }) => (
+export const Footer = () => {
+
+  const styledIcons = [
+    { styledIcon: <StyledGitHub />, link: "https://github.com/am97veb" },
+    { styledIcon: <StyledLinkedin />, link: "https://www.linkedin.com/in/am97veb" },
+    { styledIcon: <StyledFacebook />, link: "https://www.facebook.com/adrian.markefka.5" },
+    { styledIcon: <StyledInstagram />, link: "https://www.instagram.com/am97veb/" },
+    { styledIcon: <StyledX />, link: "https://x.com/AM97VEB" },
+  ];
+
+  return (
     <Wrapper>
-        <Caption>
-            let's talk
-        </Caption>
-        <Mail>
-            {mail}
-        </Mail>
-        <Description>
-            If you need help with your ideas for a website or app - go ahead, I'm always open to new projects and new challenges. Don't be shy, feel free to contact me!
-        </Description>
-        <ContactContainer>
-            <a href="https://github.com/am97veb">
-                <StyledGitHub />
-            </a>
-            <a href="https://www.linkedin.com/in/am97veb/">
-                <StyledLinkedin />
-            </a>
-            <a href="https://www.facebook.com/adrian.markefka.5">
-                <StyledFacebook />
-            </a>
-            <a href="https://www.instagram.com/am97veb/">
-                <StyledInstagram />
-            </a>
-            <a href="https://x.com/AM97VEB">
-                <StyledX />
-            </a>
-        </ContactContainer>
+      <Caption>
+        let's talk
+      </Caption>
+      <Mail href={mailTo}>
+        am97veb@gmail.com
+      </Mail>
+      <Description>
+        If you need help with your ideas for a website or app - go ahead, I'm always open to new projects and new challenges. Don't be shy, feel free to contact me!
+      </Description>
+      <ContactContainer>
+        {styledIcons.map((styledIcon) => (
+          <a
+            href={styledIcon.link}
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            {styledIcon.styledIcon}
+          </a>
+        ))}
+      </ContactContainer>
     </Wrapper>
-);
+
+  )
+};
