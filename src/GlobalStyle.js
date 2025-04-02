5fb20dedbe98ee79ffc8ad7c90918bdb8725f161
Adrian Markefka <am97veb@gmail.com>
2025-04-02 13:43:10 +0200

    improved code formatting

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,21 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-html {
-    box-sizing: border-box;
-}
+    html {
+        box-sizing: border-box;
+    }
 
-*, ::after, ::before {
-    box-sizing: inherit;
-}
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
 
-body {
-    max-width: 1920px;
-    margin: 0 auto;
-    background-color: ${({ theme }) => theme.colors.bodyBackground};
-    transition: ${({disabledTransition}) => disabledTransition ?  "none" : "0.5s"};
-    font-family: "Inter";
-    word-break: break-word;  
-    letter-spacing: 1px;
-}
+    body {
+        max-width: 1920px;
+        margin: 0 auto;
+        background-color: ${({ theme }) => theme.colors.bodyBackground};
+        transition: ${({ disabledTransition }) => disabledTransition ? "none" : "0.5s"};
+        font-family: "Inter";
+        word-break: break-word;  
+        letter-spacing: 1px;
+    }
 `;
