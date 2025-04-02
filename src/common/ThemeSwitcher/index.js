6034bd6416c06bf1bfa0fb6f8992f1d8f3ca79c9
Adrian Markefka <am97veb@gmail.com>
2025-04-02 15:46:30 +0200

    improved code formatting

--- src/common/ThemeSwitcher/index.js
+++ src/common/ThemeSwitcher/index.js
@@ -3,25 +3,25 @@ import { Wrapper, Caption, ThemeButton, ButtonWrapper, StyledButtonDisc, StyledB
 import { selectTheme, toggleTheme } from "../../features/personalHomepage/homepageSlice";
 
 export const ThemeSwitcher = () => {
-    const darkTheme = useSelector(selectTheme);
-    const dispatch = useDispatch();
-    
-    return (
-        <Wrapper>
-            <Caption>
-                Dark mode {darkTheme ? "on" : "off"}
-            </Caption>
-            <ThemeButton
-                onClick={() => dispatch(toggleTheme())}
-            >
-                <ButtonWrapper>
-                    <StyledButtonDisc
-                    $darkTheme={darkTheme}
-                    />
-                    <StyledButtonSun
-                    $darkTheme={darkTheme}/>
-                </ButtonWrapper>
-            </ThemeButton>
-        </Wrapper>
-    )
+  const darkTheme = useSelector(selectTheme);
+  const dispatch = useDispatch();
+
+  return (
+    <Wrapper>
+      <Caption>
+        Dark mode {darkTheme ? "on" : "off"}
+      </Caption>
+      <ThemeButton
+        onClick={() => dispatch(toggleTheme())}
+      >
+        <ButtonWrapper>
+          <StyledButtonDisc
+            $darkTheme={darkTheme}
+          />
+          <StyledButtonSun
+            $darkTheme={darkTheme} />
+        </ButtonWrapper>
+      </ThemeButton>
+    </Wrapper>
+  )
 };
