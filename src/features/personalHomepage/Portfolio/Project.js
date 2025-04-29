7d28294bdd0cc1304c9d653cf788a524c8cbe022
Adrian Markefka <am97veb@gmail.com>
2025-04-29 13:11:31 +0200

    added condition to "Demo"

--- src/features/personalHomepage/Portfolio/Project.js
+++ src/features/personalHomepage/Portfolio/Project.js
@@ -1,34 +1,40 @@
 import { StyledProject, ProjectName, ProjectDescription, Link, ProjectLinks, LinkText, ProjectValue, ProjectRow } from "./styled";
 
 export const Project = ({ project }) => (
-    <StyledProject>
-        <ProjectName>
-            {project.name.replaceAll("-", " ")}
-        </ProjectName>
-        <ProjectDescription>
-            {project.description}
-        </ProjectDescription>
-        <ProjectLinks>
-            <dt>
-                Demo:
-            </dt>
-            <ProjectValue>
-                <Link href={project.homepage}
-                    rel="noreferrer noopener"
-                    target="_blank">
-                    {project.homepage}
-                </Link>
-            </ProjectValue>
-            <dt>
-                Code:
-            </dt>
-            <ProjectValue>
-                <Link href={project.html_url}
-                    rel="noreferrer noopener"
-                    target="_blank">
-                    {project.html_url}
-                </Link>
-            </ProjectValue>
-        </ProjectLinks>
-    </StyledProject>
+   <StyledProject>
+      <ProjectName>
+         {project.name.replaceAll("-", " ")}
+      </ProjectName>
+      <ProjectDescription>
+         {project.description}
+      </ProjectDescription>
+      <ProjectLinks>
+         {project.homepage &&
+            (
+               <>
+                  <dt>
+                     Demo:
+                  </dt>
+                  <ProjectValue>
+                     <Link href={project.homepage}
+                        rel="noreferrer noopener"
+                        target="_blank">
+                        {project.homepage}
+                     </Link>
+                  </ProjectValue>
+               </>
+            )
+         }
+         <dt>
+            Code:
+         </dt>
+         <ProjectValue>
+            <Link href={project.html_url}
+               rel="noreferrer noopener"
+               target="_blank">
+               {project.html_url}
+            </Link>
+         </ProjectValue>
+      </ProjectLinks>
+   </StyledProject>
 );
